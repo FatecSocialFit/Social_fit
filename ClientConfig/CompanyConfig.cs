043ebb320c11e348483551a1685db67fa8363806
Joao Vitor Batista Marques <joaovitorbm08@gmail.com>
2021-05-26 20:39:25 -0300

    Adicionado CRUD das empresas

---
+++ ClientConfig/CompanyConfig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SocialFit.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialFit.ClientConfig
+{
+	public class CompanyConfig : IEntityTypeConfiguration<Company>
+	{
+		public void Configure(EntityTypeBuilder<Company> builder)
+		{
+		}
+	}
+}
---
+++ Pages/Companies/Edit.cshtml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SocialFit.Data;
+using SocialFit.Models;
+
+namespace SocialFit.Pages.Companies
+{
+    public class EditModel : PageModel
+    {
+        private readonly SocialFit.Data.SocialFitContext _context;
+
+        public EditModel(SocialFit.Data.SocialFitContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Company Company { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Company = await _context.Companies.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Company == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Company).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CompanyExists(Company.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool CompanyExists(int id)
+        {
+            return _context.Companies.Any(e => e.Id == id);
+        }
+    }
+}
